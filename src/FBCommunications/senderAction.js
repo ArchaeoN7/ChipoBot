59601a46e60664edfcc951b2f8dbc44c9cbe8808
ArchaeoN7 <rpaullouis@yahoo.fr>
2020-10-07 16:29:07 +0200

    version 2.5 - message_read notification

--- src/FBCommunications/senderAction.js
+++ src/FBCommunications/senderAction.js
@@ -3,26 +3,26 @@ const request = require('request');
 const ACCESS_TOKEN = process.env.FB_WEBHOOK_ACCESS_TOKEN
 
 
-module.exports = {
 
-    sendReadNotification(sender) {
-        console.log("on envoie la sauce")
-        request({
-            url: 'https://graph.facebook.com/v2.6/me/messages',
-            qs: {access_token: ACCESS_TOKEN},
-            method: 'POST',
-            json: {
-                recipient: {id:sender},
-                sender_action: "mark_seen"
-            }
-        }, function(error, response, body) {
-            if (error) {
-                console.log('Error sending mark_seen indicator: ', error)
-            } else if (response.body.error) {
-                console.log('Error: ', response.body.error)
-            }
-            else
-                console.log(response)
-        })
-    }
+
+const sendReadNotification = (sender) => {
+    console.log("on envoie la sauce")
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token: ACCESS_TOKEN},
+        method: 'POST',
+        json: {
+            recipient: {id:sender},
+            sender_action: "mark_seen"
+        }
+    }, function(error, response, body) {
+        if (error) {
+            console.log('Error sending mark_seen indicator: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        }
+        else
+            console.log(response)
+    })
 }
+module.exports = sendReadNotification;
